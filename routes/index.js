cb057108acfa00e91abdb072ef5b7bb3c8ab0876
wk26 <wk26@fresnou.org>
2022-03-09 08:55:24 -0800

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,13 @@ router.get('/marketing', pageController.renderMarketing);
 router.get('/sales',pageController.renderSales);
 router.get('/hr', pageController.renderHR);
 
+router.get('/profile', pageController.viewProfile);
+
 router.get('/register', userController.renderRegistrationForm);
 router.post('/register', userController.registerUser);
-router.get('/login', userController.renderLoginForm);
 
+router.get('/login', userController.renderLoginForm);
 router.post('/login', userController.loginUser);
+
+router.get('/logout', userController.logout);
 module.exports = router;
